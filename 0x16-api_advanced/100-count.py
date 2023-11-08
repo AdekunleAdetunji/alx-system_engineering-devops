21ede4aa5ce2a5f5f2d3cb155ee7e4d9a2fbfb56
adetunji <adekunleadetunjiwilson@gmail.com>
2023-11-08 03:26:42 +0100

    solution to advanced task

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module contains a recursive function that returns the list of all hot
+articles for a given subreddit
+"""
+import requests
+
+
+def count_words(subreddit, word_list, after="", dic={}):
+    """
+    recursive function that returns frequency of certain words in all titles of
+    articles linked to a subreddit
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    headers = {"user-agent": "hsld"}
+    payload = {"after": after}
+    response = requests.get(url, headers=headers,
+                            params=payload, allow_redirects=False)
+    if response.status_code != 200:
+        return None
+
+    data = response.json().get("data")
+    for child in data.get("children"):
+        title_words = child.get("data").get("title").lower().split()
+        for word in word_list:
+            dic[word] = dic.get(word, 0) + title_words.count(word.lower())
+    after = data.get("after")
+
+    if after:
+        count_words(subreddit, word_list, after, dic)
+
+    else:
+        word_list.sort(key=lambda x: dic.get(x), reverse=True)
+        for word in word_list:
+            if dic.get(word):
+                print("{}: {}".format(word, dic.get(word)))
