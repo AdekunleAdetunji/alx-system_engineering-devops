eebdf9f7627867fd0186b11b71b699380f389202
adetunji <adekunleadetunjiwilson@gmail.com>
2023-11-16 06:49:54 +0100

    solution to advanced task

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,13 @@
 # This puppet file is used to Change the OS configuration so that it is possible
 # to login with the holberton user and open a file without any error message.
 
-exec {'command':
+exec {'replace-1':
   provider => shell,
-  command  => 'sudo sed -i "s#(5|4)#10000#g" /etc/security/limits.conf',
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
+}
+
+exec {'replace-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
